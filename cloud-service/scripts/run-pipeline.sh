e5baae19887557a11f1f1095ec52b57bcc8e8cee
Suren Sritharan <bharath@ca.ibm.com>
2026-09-02 16:28:43 -0400

    fix: show full dataset-label in pipeline monitor output

--- cloud-service/scripts/run-pipeline.sh
+++ cloud-service/scripts/run-pipeline.sh
@@ -563,7 +563,7 @@ while true; do
     (.current_scenario // "") as $cur |
     (.scenarios // []) as $scens |
     ( if $cur == "" then ""
-      else ( $scens[] | select( (.dataset + "-" + .label) == $cur ) | .label ) // $cur
+      else ( $scens[] | select( (.dataset + "-" + .label) == $cur ) | (.dataset + "-" + .label) ) // $cur
       end ) as $label |
     ([ .scenario_status // {} | to_entries[] | select(.value | test("completed|failed|partial_failure|error|cancelled")) ] | length) as $done |
     ([ .scenarios // [] | .[] ] | length) as $total |
